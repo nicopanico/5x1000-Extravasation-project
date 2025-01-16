4391e85de20fe3133b303db62c63f5027b521cd6
nicopanico <nicopanico@hotmail.it>
2025-01-16 16:12:38 +0100

    first remodulation of the code

---
+++ config.py
@@ -0,0 +1,21 @@
+# config.py
+
+# Percorsi dei dati
+ROOT_CONTROLATERALS = "C:/Users/39333/OneDrive/Desktop/FUORIVENA 5x1000/controlaterals"
+ROOT_INJECTIONS = "C:/Users/39333/OneDrive/Desktop/FUORIVENA 5x1000/injections"
+PATH_GRAFICI = "C:/Users/39333/OneDrive/Desktop/FUORIVENA 5x1000/grafici"
+
+# Parametri CSV
+SEPARATOR = ";"
+ENCODING = "ISO-8859-1"
+
+# Colonne di interesse
+COLUMNS_INJECTION = {
+    'time': 'Timestamp',
+    'value': 'Intensità di dose (μSv/h)'
+}
+
+COLUMNS_CONTROLATERAL = {
+    'time': 'Timestamp',
+    'value': 'Intensità di dose (μSv/h)'
+}
