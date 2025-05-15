5571b6f6a33593059dd8b1b83b276c30449ac56a
nicopanico <nicopanico@hotmail.it>
2025-05-15 16:09:10 +0200

    v2 patch -- modified the diagnsotic analysis method to better process peak-data

--- config.py
+++ config.py
@@ -4,21 +4,28 @@ import os
 
 THIS_FILE = os.path.abspath(__file__)
 MAIN_DIR = os.path.dirname(THIS_FILE)
-
-# la directory del progetto è il genitore della cartella "code".
 PROJECT_DIR = os.path.dirname(MAIN_DIR)
 
-# Percorsi per la terapia
-ROOT_INJECTIONS_THERAPY = os.path.join(PROJECT_DIR, "injections")
-ROOT_CONTROLATERALS_THERAPY = os.path.join(PROJECT_DIR, "controlaterals")
-PATH_GRAFICI_THERAPY = os.path.join(PROJECT_DIR, "grafici")
+# -----------------------------
+# 1) Cartelle per la Terapia
+# -----------------------------
+THERAPY_DIR = os.path.join(PROJECT_DIR, "Terapia")
 
-# Percorsi per la diagnostica
-ROOT_INJECTIONS_DIAGNOSTIC = os.path.join(PROJECT_DIR, "stravaso_processed", "injections")
-ROOT_CONTROLATERALS_DIAGNOSTIC = os.path.join(PROJECT_DIR, "stravaso_processed", "controlaterals")
-PATH_GRAFICI_DIAGNOSTIC = os.path.join(PROJECT_DIR, "stravaso_processed", "grafici")
+ROOT_INJECTIONS_THERAPY = os.path.join(THERAPY_DIR, "injections")
+ROOT_CONTROLATERALS_THERAPY = os.path.join(THERAPY_DIR, "controlaterals")
+PATH_GRAFICI_THERAPY = os.path.join(THERAPY_DIR, "grafici")
 
-# Parametri CSV comuni
+# -----------------------------
+# 2) Cartelle per la Diagnostica
+# -----------------------------
+DIAGNOSTIC_DIR = os.path.join(PROJECT_DIR, "Diagnostica")
+STRAVASO_PROCESSED_DIR = os.path.join(DIAGNOSTIC_DIR, "stravaso_processed")
+
+ROOT_INJECTIONS_DIAGNOSTIC = os.path.join(STRAVASO_PROCESSED_DIR, "injections")
+ROOT_CONTROLATERALS_DIAGNOSTIC = os.path.join(STRAVASO_PROCESSED_DIR, "controlaterals")
+PATH_GRAFICI_DIAGNOSTIC = os.path.join(STRAVASO_PROCESSED_DIR, "grafici")
+
+# Parametri CSV
 SEPARATOR = ","
 ENCODING = "utf-8"
 
@@ -29,7 +36,6 @@ COLUMNS_INJECTION_THERAPY = {
     'intensity': 'Intensità di dose (μSv/h)',
     'count_rate': 'Tasso conteggio (cps)'
 }
-
 COLUMNS_CONTROLATERAL_THERAPY = {
     'time': 'Timestamp',
     'dose': 'Dose (μSv)',
@@ -37,19 +43,17 @@ COLUMNS_CONTROLATERAL_THERAPY = {
     'count_rate': 'Tasso conteggio (cps)'
 }
 
-# Colonne di interesse per la diagnostica  
+# Colonne di interesse per la diagnostica
 COLUMNS_INJECTION_DIAGNOSTIC = {
     'time': 'timestamp',
     'dose_rate': 'dose_rate'
 }
-
 COLUMNS_CONTROLATERAL_DIAGNOSTIC = {
     'time': 'timestamp',
     'dose_rate': 'dose_rate'
 }
 
 # Modalità di analisi: "therapy" o "diagnostic"
-ANALYSIS_MODE = "therapy"  # Cambia in "diagnostic" se vuoi analizzare dati diagnostici
-
+ANALYSIS_MODE = "therapy"  # Cambia in "therapy" se desideri analizzare i dati di Terapia
 
 
